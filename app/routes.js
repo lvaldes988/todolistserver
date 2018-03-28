6d2dab3e7e9c9c4bcc0ea6896ba001382bda7745
liset valdes <lvaldes@techlaunch.io>
2018-03-27 20:10:59 -0400

    another commit

--- app/routes.js
+++ app/routes.js
@@ -1,76 +1,95 @@
 var moment = require('moment');
 
 module.exports = function (app, database) {
-// get requestes listening to get task , 
-    app.get('/', function (req, res) {
-        res.render('index.ejs', {
-            user: {
-                name: 'Liset Valdes'
-            }
-        });
-    });
-
-    app.get('get-tasks', function (req, res) {
-        //database.connect();
-        //sending a query to retrieve informaction 
-        //databae is a object 
-        //is a funtio n that takes two parameters the database and the query
-
-
-        database.query(
-            `SELECT * FROM todos`, 
-            function (error, results, fields) {
-      
-            if (error) throw error;
-      
-            //console.log('results: ', results);
-      
-            res.render('todos.ejs', {
-              todos: results
-            });
-          });
-      
-        });
-      
-        app.post('/create-todo', function (req, res) {
-
-          console.log('data', req.body);
-
-          var task = req.body.task;
-          
-          database.query(
-            `INSERT INTO todos(task, date, complete, uid, due_date) 
-             VALUES('${task}', '${moment().format()}', false, 'sd9f87sdf76s7d6fsdf67sd', '${moment().add(7, "days").format('YYYY/MM/DD')}')`, 
-            function (error, result, fields) {
-
-              if (error) {
-                console.log('error ', error);
-      
-                res.send({
-                  success: false,
-                  error: error
-                });
-              }
-              else {
-                console.log('result: ', result);
-      
-                res.send({
-                  success: true,
-                  result: result
-                });
-              }
-              
-          });
-      
-        });
-      
-      
-        app.post('/update-todo', function (req, res) {
-      
-        });
-      
-        app.post('/delete-todo', function (req, res) {
-      
-        });
-      
+  // get requestes listening to get task , 
+  app.get('/', function (req, res) {
+    res.render('index.ejs', {
+      user: {
+        name: 'Liset Valdes'
       }
+    });
+  });
+
+  app.get('/get-todos', function (req, res) {
+    //database.connect();
+    //sending a query to retrieve informaction 
+    //databae is a object 
+    //is a funtio n that takes two parameters the database and the query
+
+
+    database.query(
+      `SELECT * FROM todos`,
+      function (error, results, fields) {
+
+        if (error) throw error;
+
+        //console.log('results: ', results);
+
+        res.render('todos.ejs', {
+          todos: results
+        });
+      });
+
+  });
+
+  app.post('/update-todo', function (req, res) {
+    var id = req.body.id;
+
+    database.query(
+      `UPDATE todos SET complete = true WHERE id = ${id}`,
+      function (error, result, fields) {
+
+        if (error) {
+          console.log('error ', error);
+
+          res.send({
+            success: false,
+            error: error,
+            message: 'The todo was not updated :('
+          });
+        }
+        else {
+          console.log('result: ', result);
+
+          res.send({
+            success: true,
+            id: id
+          });
+        }
+
+      });
+  });
+
+  app.post('/delete-todo', function (req, res) {
+    var id = req.body.id;
+
+    database.query(
+      `DELETE FROM todos WHERE id = ${id}`,
+      function (error, result, fields) {
+
+        if (error) {
+          console.log('error ', error);
+
+          res.send({
+            success: false,
+            error: error,
+            message: 'The todo was not deleted :('
+          });
+        }
+        else {
+          console.log('result: ', result);
+
+          res.send({
+            success: true,
+            id: id
+          });
+        }
+      });
+
+  });
+
+  app.get('*', function (req, res) {
+    res.render('404.ejs');
+  });
+
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,91 @@
 $(document).ready(function(){
-    $('#btn').on('click', function(){
-      let task = $('#todo_input').val();
-      if (task) {
-        $.post("/create-todo", {
-          task: task
-        }, function (result) {
-          console.log('result ', result);
-        });
+  const scroll = () => $('#todos_container').scrollTop($('#todos_container')[0].scrollHeight);
+
+  const createTodo = function() {
+    let task = $('#todo_input').val();
+    if (task) {
+      $.post("/create-todo", {
+        task: task
+      }, function (response) {
+        if (response.success) {
+          console.log('result ', response.todo);
+
+          $('#todos_container').append(`
+            <div id="${response.todo.id}" class="todo-container ${response.todo.complete ? 'checked' : ''}">
+              <div class="remove">
+                <span class="ion-close-round icon-remove"></span>
+              </div>
+              <div data-complete="${response.todo.complete}" class="todo">
+                ${response.todo.task}
+              </div>
+            </div>
+          `);
+
+          $('#todo_input').val('');
+
+          scroll();
+        }
+        else {
+          alert(response.message);
+        }
+      });
+    }
+    else {
+      alert('You must insert a task');
+    }
+  }
+
+  const removeTodo = function(e) {
+    let id = $(this).parent()[0].id;
+
+    console.log('removing ', id);
+
+    $.post("/delete-todo", {
+      id: id
+    }, function (response) {
+      if (response.success) {
+        $('#' + response.id).remove();
       }
       else {
-        alert('You must insert a task');
+        alert(response.message);
       }
     });
+  }
+
+  const updateTodo = function (e) {
+
+    let id = $(this).parent()[0].id;
+    let complete = $(this).data('complete');
+
+    console.log('complete ', complete);
+
+    if (complete === false) {
+      $.post("/update-todo", {
+        id: id
+      }, function (response) {
+        if (response.success) {
+          $('#' + response.id).addClass('checked');
+        }
+        else {
+          alert(response.message);
+        }
+      });
+    }
+
+  }
+
+  $('#todos_container').on('click', '.remove', removeTodo);
+
+  $('#todos_container').on('click', '.todo', updateTodo);
+
+  $('#btn').on('click', createTodo);
+
+  $('#todo_input').on('keyup', function (e) {
+    if(e.keyCode === 13){
+      createTodo();
+    }
   });
+});
 
 
 
